81a4a9e4e8f1c592f0f5dc14b6743cb710093c1f
xsaven <gms.sw.xsaven@gmail.com>
2021-07-13 22:00:43 +0300

    composer.json, src/RouteServiceProvider.php

--- src/RouteServiceProvider.php
+++ src/RouteServiceProvider.php
@@ -2,17 +2,15 @@
 
 namespace Bfg\Route;
 
-use Bfg\Route\Core\ControllerDispatcher;
+use Bfg\Installer\Providers\InstalledProvider;
 use Bfg\Route\Core\RouteMixin;
-use Illuminate\Routing\Contracts\ControllerDispatcher as ControllerDispatcherContract;
 use Illuminate\Routing\Router;
-use Illuminate\Support\ServiceProvider;
 
 /**
  * Class RouteServiceProvider
  * @package Bfg\Route
  */
-class RouteServiceProvider extends ServiceProvider
+class RouteServiceProvider extends InstalledProvider
 {
     /**
      * Enable state
@@ -22,10 +20,24 @@ class RouteServiceProvider extends ServiceProvider
     protected static bool $enabled = false;
 
     /**
-     * Register route settings
+     * The description of extension.
+     * @var string|null
+     */
+    public ?string $description = "PHP 8 attributes to register routes in a Laravel";
+
+    /**
+     * Set as installed by default.
+     * @var bool
+     */
+    public bool $installed = true;
+
+    /**
+     * Executed when the provider is registered
+     * and the extension is installed.
+     * @return void
      * @throws \ReflectionException
      */
-    public function register()
+    function installed(): void
     {
         /**
          * Checks if there is a cache file for the route,
@@ -47,10 +59,20 @@ class RouteServiceProvider extends ServiceProvider
     }
 
     /**
+     * Executed when the provider run method
+     * "boot" and the extension is installed.
+     * @return void
+     */
+    function run(): void
+    {
+        //
+    }
+
+    /**
      * Enable state getter
      * @return bool
      */
-    public static function isEnabled()
+    public static function isEnabled(): bool
     {
         return static::$enabled;
     }
